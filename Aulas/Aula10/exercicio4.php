277a8f8c6084ecc298cc261297ac4a06e8b5c819
Kaua de Freitas Lucas <11268954667@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-13 08:41:46 -0300

    exercicios vetor

---
+++ Aulas/Aula10/exercicio4.php
@@ -0,0 +1,13 @@
+<h2>Exercicio 1</h2>
+
+<pre>
+<?php
+$nome = ["pedro", "julia", "maria"];
+echo "<ol>";
+   for($i=0; $i<=2; $i++){
+      echo "<li>" . $nome[$i] . "</li>";
+}
+echo "</ol>";
+
+?>
+</pre>
---
+++ Aulas/Aula10/exercicio5.php
@@ -0,0 +1,14 @@
+<h2>Exercicio 2</h2>
+
+<pre>
+<?php
+$nomes = [];
+$nomes["nome"] = "Maria";
+$nomes["idade"] = "17";
+$nomes["altura"] = "1.65";
+
+foreach($nomes as $key => $values){
+    echo $key . "<br>";
+}
+?>
+</pre>
